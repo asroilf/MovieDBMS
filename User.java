de25d78fd241adf406484254ca6e18d59101f880
codeBroh <asliddin.is17@gmail.com>
2023-11-28 17:44:25 +0400

    User.java updated with username, pass, dob, constructors, getter/setters, etc.

---
+++ User.java
@@ -0,0 +1,38 @@
+public class User{
+    private String username;
+    private String password;
+    private int[] dob;
+
+    public User(String username, String password, int[] dob){
+        this.username = username;
+        this.password = password;
+        this.dob = dob;
+    }
+    public User(String username, String password){
+        this.username = username;
+        this.password = password;
+        dob = new int[]{9,9,1999};
+    }
+
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int[] getDob() {
+        return dob;
+    }
+    public void setDob(int[] dob) {
+        this.dob = dob;
+    }
+
+}
