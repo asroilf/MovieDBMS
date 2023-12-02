8a9f468ba4b0a56d21234c4a5c681592e67f4cf0
codeBroh <asliddin.is17@gmail.com>
2023-12-02 23:18:30 +0400

    .

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+
+public class Main {
+    public static void main(String[] args) {
+        ObjectOutputStream oos = new ObjectOutputStream();
+    }
+}
