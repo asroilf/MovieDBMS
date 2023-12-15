a120646da60dcce5ea378be8e8e6f70d1f20a432
Fmammadli13936 <faridapril1982@gmail.com>
2023-12-15 23:38:16 +0400

    Remove and Add methods have been added

--- MovieDatabase.java
+++ MovieDatabase.java
@@ -8,40 +8,84 @@ public class MovieDatabase {
         movies = new ArrayList<>();
     }
 
-    public void addMovie(Movie movie) {
-        if (!movies.contains(movie)) {
-            movies.add(movie);
-            System.out.println("Movie added to the database: " + movie.getTitle());
-            // You can add file writing logic here to store the updated movie list in a file
-        } else {
-            System.out.println("Movie already exists in the database.");
+    public static void addMovie(Movie movie) {
+        try (FileOutputStream fos = new FileOutputStream(new File("DB/Movie.txt"));
+                ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+            oos.writeObject(movie);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+    }
+
+    public static void remove(String movie) {
+        // Open the file, then we read it.
+        ArrayList<Movie> al = new ArrayList<>();
+        try (FileInputStream fos = new FileInputStream(new File("DB/Movie.txt"));
+                ObjectInputStream ois = new ObjectInputStream(fos)) {
+            Movie movie4;
+            while ((movie4 = (Movie) ois.readObject()) != null) {
+                al.add(movie4);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try (ObjectOutputStream fos = new ObjectOutputStream(new FileOutputStream("DB/Movie.txt"))) {
+            for (int i = 0; i < al.size(); i++) {
+                Movie movie5 = al.get(i);
+                if (movie5.getTitle().equals(movie)) {
+                    continue;
+                } else {
+                    fos.writeObject(movie5);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static Movie retrieveMovie(String title) {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream("DB/Movie.txt"))) {
+            Movie m;
+
+            while ((m = (Movie) ois.readObject()) != null) {
+                if (m.getTitle().equals(title)) {
+                    return m;
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Movie doesn't exist");
+        }
+
+        return new Movie();
+
     }
 
     public void removeMovie(Movie movie) {
         if (movies.contains(movie)) {
             movies.remove(movie);
-            System.out.println("Movie removed from the database: " + movie.getTitle());
-            // Here aswell
+            System.out.println(movie.getTitle());
         } else {
-            System.out.println("Movie does not exist in the database.");
+            System.out.println("Movie doesn't exist.");
         }
     }
 
-    public Movie getMovieByTitle(String title) {
+    public Movie getMovieTitle(String title) {
         for (Movie movie : movies) {
             if (movie.getTitle().equalsIgnoreCase(title)) {
                 return movie;
             }
         }
-        return null; 
+        return null;
     }
 
-    public void displayAllMovies() {
+    public void AllMovies() {
         System.out.println("List of Movies in the Database:");
         for (Movie movie : movies) {
             System.out.println(movie);
             System.out.println("----------------------");
         }
     }
+
 }
--- User.java
+++ User.java
@@ -5,58 +5,62 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Iterator;
 
-public class User implements Serializable{
+public class User implements Serializable {
     private String username;
     private String name;
     private String password;
     private int[] dob;
-    private static int countUsers=0;
+    private static int countUsers = 0;
 
-    public User(String username, String name, String password, int[] dob){
+    public User(String username, String name, String password, int[] dob) {
         this.username = username;
         this.name = name;
         this.password = password;
         this.dob = dob;
         countUsers++;
     }
-    public User(String username, String password){
+
+    public User(String username, String password) {
         this.username = username;
         this.name = "John Wick";
         this.password = password;
-        dob = new int[]{9,9,1999};
+        dob = new int[] { 9, 9, 1999 };
         countUsers++;
     }
 
-    public User(){
+    public User() {
         this.username = "void";
         this.name = "John Wick";
         this.password = "password";
-        dob = new int[]{9,9,1999};
+        dob = new int[] { 9, 9, 1999 };
         countUsers++;
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         return this.name + ", born in: " + dob[2];
     }
 
     public String getUsername() {
         return username;
     }
+
     public void setUsername(String username) {
         this.username = username;
     }
-    
-    public String getName(){
+
+    public String getName() {
         return name;
     }
-    public void setName(String name){
+
+    public void setName(String name) {
         this.name = name;
     }
 
     public String getPassword() {
         return password;
     }
+
     public void setPassword(String password) {
         this.password = password;
     }
@@ -64,36 +68,37 @@ public class User implements Serializable{
     public int[] getDob() {
         return dob;
     }
+
     public void setDob(int[] dob) {
         this.dob = dob;
     }
 
-    public int getCountUser(){
+    public int getCountUser() {
         return countUsers;
     }
 
-    public static int login(String username, String password) throws ValidationException{
+    public static int login(String username, String password) {
         ArrayList<User> al = new ArrayList<>();
         try (FileInputStream fr = new FileInputStream("DB/UserFile.txt");
-            ObjectInputStream dis = new ObjectInputStream(fr)) {
+                ObjectInputStream dis = new ObjectInputStream(fr)) {
             User temp;
-            while((temp = (User)dis.readObject()) != null ){
+            while ((temp = (User) dis.readObject()) != null) {
                 al.add(temp);
                 System.out.println(temp.getName() + ", " + temp.getUsername() + ", pass: " + temp.getPassword());
             }
-        } catch (EOFException e){
+        } catch (EOFException e) {
             System.out.println("End of the file!");
-        }catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
         Iterator<User> iter = al.iterator();
-        while(iter.hasNext()){
+        while (iter.hasNext()) {
             User u = iter.next();
-            if(u.getUsername().equals(username) && u.getPassword().equals(password)){
+            if (u.getUsername().equals(username) && u.getPassword().equals(password)) {
                 return 1;
             }
         }
-        throw new ValidationException("Incorrect username and/or password, better luck next time!");
+        return -1;
     }
-  
-} 
+
+}
