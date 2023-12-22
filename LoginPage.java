a87c279014bd0726a8d7eea7eb527d19130c223f
codeBroh <asliddin.is17@gmail.com>
2023-12-22 23:48:29 +0400

    working on authentication system

--- LoginPage.java
+++ LoginPage.java
@@ -1,40 +1,85 @@
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
 import javax.swing.JScrollBar;
 import javax.swing.JTextField;
 
-// public class LoginPage extends JFrame {
+    public class LoginPage extends JFrame implements ActionListener{
+        JTextField username;
+        JPasswordField password;
+        JButton login;
 
-//     public LoginPage() {
-//         // Window setep hd
-//         this.setVisible(true);
-//         this.setSize(1920, 1080);
-//         this.setTitle("Local Movie Database");
-//         this.getComponents();
-//         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        public LoginPage() {
+            // Window setep hd
+            this.setVisible(true);
+            this.setSize(1920, 1080);
+            this.setTitle("Local Movie Database");
+            this.getComponents();
+            this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    
+            // panel for login button both implementing ActionListener Method
+            login = new JButton("Login");
+    
+            username = new JTextField();
+            password = new JPasswordField();
+            JPanel jp1 = new JPanel();
+            username.setPreferredSize(new Dimension(200, 30));
+            password.setPreferredSize(new Dimension(200, 30));
+    
+            jp1.setSize(300, 200);
+            jp1.add(username);
+            jp1.add(password);
+            jp1.add(login);
+    
+            // this.add(jp);
+            this.add(jp1);
+        }
 
-//         // panel for login button both implementing ActionListener Method
-//         JButton login = new JButton("Login");
-//         // login.addActionListener(this);
-//         // register.setBounds(100, 10, 50, 50);
-//         JPanel jp = new JPanel();
-//         jp.setSize(300, 200);
-//         jp.add(login);
-//         this.add(jp);
+        public LoginPage(String str){
+            
+            this.setVisible(true);
+            this.setSize(1920, 1080);
+            this.setTitle("Local Movie Database");
+            this.getComponents();
+            this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    
+            
+            login = new JButton("Login");
+    
+            username = new JTextField();
+            password = new JPasswordField();
+            JPanel jp1 = new JPanel();
+            username.setPreferredSize(new Dimension(200, 30));
+            password.setPreferredSize(new Dimension(200, 30));
+    
+            jp1.setSize(300, 200);
+            jp1.add(username);
+            jp1.add(password);
+            jp1.add(login);
+    
+            // this.add(jp);
+            this.add(jp1);
+        }
 
-//         JTextField tf = new JTextField();
-//         JPanel jp1 = new JPanel();
-//         tf.setPreferredSize(new Dimension(200, 30));
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if(e.getSource() == login){
+                String uname = username.getText();
+                String pass = password.getSelectedText();
+                int status = User.login(uname, pass);
+                if(status == 1){
+                    new LoginPage();
+                }
+                else{
+                    new LoginPage("Not valid");
+                }
+            }
+        }
 
-//         jp1.setSize(300, 200);
-//         jp1.add(tf);
-
-//         this.add(jp);
-//         this.add(jp1);
-
-//     }
-// }
+    }
--- ValidationException.java
+++
@@ -1,5 +0,0 @@
-public class ValidationException extends Exception {
-    public ValidationException(){
-        super();
-    }
-}
